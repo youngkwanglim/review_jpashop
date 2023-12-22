30c0700166d664aadaf00a6194b8d7cf71379e82
Youngkwang Lim <richard08@naver.com>
2023-12-23 03:03:58 +0900

    1. 프로젝트 환경설정

--- src/test/java/review_jpabook/review_jpashop/MemberRepositoryTest.java
+++ src/test/java/review_jpabook/review_jpashop/MemberRepositoryTest.java
@@ -33,7 +33,7 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
