e80714c8c76a4b7e2bdbb6d3206ab19c4627e2b1
Youngkwang Lim <richard08@naver.com>
2024-01-03 23:11:51 +0900

    활용2 - 5. API 개발 고급 - 실무 필수 최적화

---
+++ src/main/java/review_jpabook/review_jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,8 @@
+package review_jpabook.review_jpashop.service.query;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public class OrderQueryService {
+
+}
